ee1209b088ce4bfd635ee2c7870cf72d89a24c82
Tiago Poli <tiagoluizpoli@gmail.com>
2025-09-17 01:12:35 -0300

    add theme provider

--- frontend/src/providers/providers.tsx
+++ frontend/src/providers/providers.tsx
@@ -4,18 +4,21 @@ import { RouterProvider } from 'react-router-dom';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { env } from '@/config';
 import { router } from '@/routes';
+import { ThemeProvider } from './theme-provider';
 
 export const Providers = () => {
   const { VITE_CLERK_PUBLISHABLE_KEY } = env;
   const queryClient = new QueryClient();
 
   return (
-    <ClerkProvider publishableKey={VITE_CLERK_PUBLISHABLE_KEY}>
-      <QueryClientProvider client={queryClient}>
-        <SidebarProvider>
-          <RouterProvider router={router} />
-        </SidebarProvider>
-      </QueryClientProvider>
-    </ClerkProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ClerkProvider publishableKey={VITE_CLERK_PUBLISHABLE_KEY}>
+        <QueryClientProvider client={queryClient}>
+          <SidebarProvider>
+            <RouterProvider router={router} />
+          </SidebarProvider>
+        </QueryClientProvider>
+      </ClerkProvider>
+    </ThemeProvider>
   );
 };
